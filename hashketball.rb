2872e833d19ac66cd0888622b7073a04b5873040
Bree Warren <ambreea.warren@gmail.com>
2020-05-27 03:23:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,19 @@
 def game_hash
   hashketball = {
     home: {
-      team_name: "",
-      colors: [],
-      players: []
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [{
+        player_name: "",
+        number: "",
+        shoe: "",
+        points: "",
+        rebounds: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slam_dunks: ""
+        }]
     },
     away: {
       team_name: "",
